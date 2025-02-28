811095af07e40960578f6cf5ace8396123afbe8e
Team 9567 Programing <Team9567Prog@gmail.com>
2025-02-28 08:33:29 -0800

    Add USB camera initialization

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,29 +4,29 @@
 
 package frc.robot;
 
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.Constants.ControllerConstants;
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.Constants.RollerConstants;
-import frc.robot.Constants.RobotConstants;
-import frc.robot.Constants.AlgaeConstants;
 import frc.robot.Constants.ButtonConstants;
 import frc.robot.Constants.ChassisConstants;
+import frc.robot.Constants.ControllerConstants;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.Constants.RobotConstants;
+import frc.robot.Constants.RollerConstants;
 import frc.robot.commands.Autos;
-import frc.robot.commands.ReachAndGrab;
 import frc.robot.commands.DriveDistanceCommand;
+import frc.robot.commands.ReachAndGrab;
 import frc.robot.commands.TurnToAngle;
+import frc.robot.subsystems.AlgaePickerSubsystem;
 import frc.robot.subsystems.ChassieSubSystem;
 import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.CoralRoller;
-import frc.robot.subsystems.AlgaePickerSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -60,6 +60,8 @@ public class RobotContainer {
       m_coralRoller = new CoralRoller();
       m_algaePicker = new AlgaePickerSubsystem();
       m_climber = new Climber();
+      // Creates UsbCamera
+      CameraServer.startAutomaticCapture();
     }
     // Configure the trigger bindings
     configureBindings();
