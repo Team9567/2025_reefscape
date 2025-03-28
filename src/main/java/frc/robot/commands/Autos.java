bd9175bac5b72354efb62d806d19ef386bfd0246
Team 9567 Programming <team9567prog@gmail.com>
2025-03-27 19:54:56 -0700

    Made algaebat auto, tweaked timeout times, and created new Constants

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -12,31 +12,33 @@ import frc.robot.subsystems.ChassieSubSystem;
 import frc.robot.subsystems.CoralRoller;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 
 public final class Autos {
   /** Example static factory for an autonomous command. */
   public static Command simpleAutoLeft(ChassieSubSystem subsystem, CoralRoller coralSubsystem) {
     if (coralSubsystem != null) {
       return new DriveDistanceCommand(AutosConstants.k_leftDist1, subsystem)
-        .andThen(new TurnToAngle(AutosConstants.k_leftAngle1, subsystem))
-        .andThen(new DriveDistanceCommand(AutosConstants.k_leftDist2, subsystem))
-        .andThen(coralSubsystem.runRoller(coralSubsystem, () -> AutosConstants.k_rollerForwardSpeed, () -> AutosConstants.k_rollerReverseSpeed));
-    }
-    else {
+          .andThen(new TurnToAngle(AutosConstants.k_leftAngle1, subsystem))
+          .andThen(new DriveDistanceCommand(AutosConstants.k_leftDist2, subsystem))
+          .andThen(coralSubsystem.runRoller(coralSubsystem, () -> AutosConstants.k_rollerForwardSpeed,
+              () -> AutosConstants.k_rollerReverseSpeed));
+    } else {
       return new DriveDistanceCommand(AutosConstants.k_leftDist1, subsystem)
-        .andThen(new TurnToAngle(AutosConstants.k_leftAngle1, subsystem))
-        .andThen(new DriveDistanceCommand(AutosConstants.k_leftDist2, subsystem));
+          .andThen(new TurnToAngle(AutosConstants.k_leftAngle1, subsystem))
+          .andThen(new DriveDistanceCommand(AutosConstants.k_leftDist2, subsystem));
     }
   }
 
   public static Command simpleAutoMiddle(ChassieSubSystem subsystem, CoralRoller coralSubsystem) {
     if (coralSubsystem != null) {
       return new DriveDistanceCommand(AutosConstants.k_middleDist1, subsystem)
-      .withTimeout(4.0)
-        .andThen(coralSubsystem.runRoller(coralSubsystem, () -> AutosConstants.k_rollerForwardSpeed, () -> AutosConstants.k_rollerReverseSpeed));
-    }
-    else {
+          .withTimeout(2.0)
+          .andThen(coralSubsystem.runRoller(coralSubsystem, () -> AutosConstants.k_rollerForwardSpeed,
+              () -> AutosConstants.k_rollerReverseSpeed));
+    } else {
       return new DriveDistanceCommand(AutosConstants.k_middleDist1, subsystem);
     }
   }
@@ -44,71 +46,65 @@ public final class Autos {
   public static Command simpleAutoSide(ChassieSubSystem subsystem, CoralRoller coralSubsystem) {
     if (coralSubsystem != null) {
       return new DriveDistanceCommand(AutosConstants.k_sideDist1, subsystem)
-      .withTimeout(4.0)
-        .andThen(coralSubsystem.runRoller(coralSubsystem, () -> AutosConstants.k_rollerForwardSpeed, () -> AutosConstants.k_rollerReverseSpeed).withTimeout(1.0));
-    }
-    else {
+          .withTimeout(4.0)
+          .andThen(coralSubsystem.runRoller(coralSubsystem, () -> AutosConstants.k_rollerForwardSpeed,
+              () -> AutosConstants.k_rollerReverseSpeed).withTimeout(1.0));
+    } else {
       return new DriveDistanceCommand(AutosConstants.k_sideDist1, subsystem);
     }
   }
 
-  public static Command sidePlusCoral(ChassieSubSystem subsystem, CoralRoller coralSubsystem) {
-    if (coralSubsystem != null) {
-      return simpleAutoSide(subsystem, coralSubsystem)
-        .andThen(new DriveDistanceCommand(AutosConstants.k_fromReefDist1, subsystem))
-        .andThen(new TurnToAngle(AutosConstants.k_fromReefAngle1, subsystem))
-        .andThen(new DriveDistanceCommand(AutosConstants.k_toSourceDist1, subsystem))
-        .andThen(new TurnToAngle(AutosConstants.k_toSourceAngle1, subsystem))
-        .andThen(new DriveDistanceCommand(AutosConstants.k_toSourceDist2, subsystem))
-        // TO-DO - wait for coral
-        .andThen(new DriveDistanceCommand(AutosConstants.k_fromSourceDist1, subsystem))
-        .andThen(new TurnToAngle(AutosConstants.k_fromSourceAngle1, subsystem))
-        .andThen(new DriveDistanceCommand(AutosConstants.k_toReefDist1, subsystem))
-        .andThen(new TurnToAngle(AutosConstants.k_toReefAngle1, subsystem))
-        .andThen(new DriveDistanceCommand(AutosConstants.k_toReefDist2, subsystem))
-        .andThen(coralSubsystem.runRoller(coralSubsystem, () -> AutosConstants.k_rollerForwardSpeed, () -> AutosConstants.k_rollerReverseSpeed));
-    }
-    else {
-      return new DriveDistanceCommand(AutosConstants.k_sideDist1, subsystem);
-    }
+  public static Command knockAlgae(ChassieSubSystem subSystem) {
+    ParallelCommandGroup turnAndRaiseBat = new ParallelCommandGroup(
+        new TurnToAngle(AutosConstants.k_algaebatTurn, subSystem),
+        // This is a stand-in for raiseBat
+        new InstantCommand(() -> {
+        }));
+
+    ParallelCommandGroup swingBatAndRun = new ParallelCommandGroup(
+        // This is a stand-in for swingBat command
+        new InstantCommand(() -> {
+        }),
+        new InstantCommand(() -> {
+        }).withTimeout(1).andThen(new DriveDistanceCommand(AutosConstants.k_algaebatDist2, subSystem).withTimeout(1)));
+
+    return new DriveDistanceCommand(AutosConstants.k_algaebatDist1, subSystem).withTimeout(1)
+        .andThen(turnAndRaiseBat)
+        .andThen(new DriveDistanceCommand(AutosConstants.k_algaebatDist1, subSystem)
+            .withTimeout(1))
+        .andThen(swingBatAndRun);
   }
-  public static Command knockAlgae(ChassieSubSystem subSystem){
-    return new DriveDistanceCommand(AutosConstants.k_middleDist1, subsystem);
-    .andThen(new TurnToAngle())
-    .andThen(new DriveDistanceCommand(AutosConstants.k_middleDist1, subsystem));
-    .andThen(new DriveDistanceCommand(AutosConstants.k_middleDist1, subsystem));
-  }
-  public static Command midCoralPlusAlgae(ChassieSubSystem subSystem, CoralRoller coralSubsystem){
+
+  public static Command midCoralPlusAlgae(ChassieSubSystem subSystem, CoralRoller coralSubsystem) {
     if (coralSubsystem != null) {
       return simpleAutoMiddle(subSystem, coralSubsystem)
-        .andThen(knockAlgae(subSystem));
+          .andThen(knockAlgae(subSystem));
 
-  }
-  
-  
-  
+    } else {
+      return new DriveDistanceCommand(AutosConstants.k_sideDist1, subSystem);
+    }
+
   }
 
   public static Command simpleAutoRight(ChassieSubSystem subsystem, CoralRoller coralSubsystem) {
     if (coralSubsystem != null) {
       return new DriveDistanceCommand(AutosConstants.k_rightDist1, subsystem)
-        .andThen(new TurnToAngle(AutosConstants.k_rightAngle1, subsystem))
-        .andThen(new DriveDistanceCommand(AutosConstants.k_rightDist2, subsystem))
-        .andThen(coralSubsystem.runRoller(coralSubsystem, () -> AutosConstants.k_rollerForwardSpeed, () -> AutosConstants.k_rollerReverseSpeed));
-    }
-    else {
+          .andThen(new TurnToAngle(AutosConstants.k_rightAngle1, subsystem))
+          .andThen(new DriveDistanceCommand(AutosConstants.k_rightDist2, subsystem))
+          .andThen(coralSubsystem.runRoller(coralSubsystem, () -> AutosConstants.k_rollerForwardSpeed,
+              () -> AutosConstants.k_rollerReverseSpeed));
+    } else {
       return new DriveDistanceCommand(AutosConstants.k_rightDist1, subsystem)
-        .andThen(new TurnToAngle(AutosConstants.k_rightAngle1, subsystem))
-        .andThen(new DriveDistanceCommand(AutosConstants.k_rightDist2, subsystem));
+          .andThen(new TurnToAngle(AutosConstants.k_rightAngle1, subsystem))
+          .andThen(new DriveDistanceCommand(AutosConstants.k_rightDist2, subsystem));
     }
   }
 
   public static Command simpleAuto(ChassieSubSystem subsystem, CoralRoller coralSubsystem) {
     if (coralSubsystem != null) {
       return new DriveDistanceCommand(AutosConstants.k_middleDist1, subsystem)
-      .withTimeout(4.0);
-    }
-    else {
+          .withTimeout(4.0);
+    } else {
       return new DriveDistanceCommand(AutosConstants.k_middleDist1, subsystem);
     }
   }
