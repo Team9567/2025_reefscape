4dbda68010aadd53d7dfe0181e924d5e6e27462b
Team 9567 Programing <Team9567Prog@gmail.com>
2025-11-04 19:04:36 -0800

    Added pid controller to AlgaeBat, Constants to disable subsystems, disabeled Climber and AlgaePickerSubsystem

--- src/main/java/frc/robot/subsystems/AlgaePickerSubsystem.java
+++ src/main/java/frc/robot/subsystems/AlgaePickerSubsystem.java
@@ -19,59 +19,71 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.AlgaeConstants;
 
 public class AlgaePickerSubsystem extends SubsystemBase {
-    final SparkMax pivotMotor;
-    final SparkFlex intakeMotor;
-    final AbsoluteEncoder pivotEncoder;
+    SparkMax pivotMotor;
+    SparkFlex intakeMotor;
+    AbsoluteEncoder pivotEncoder;
     LaserCan algaeRanger;
 
     public AlgaePickerSubsystem() {
         // Set up the pivot and intake motors as brushless motors
-        pivotMotor = new SparkMax(AlgaeConstants.PIVOT_MOTOR_ID, MotorType.kBrushless);
-        intakeMotor = new SparkFlex(AlgaeConstants.INTAKE_MOTOR_ID, MotorType.kBrushless);
-        pivotEncoder = pivotMotor.getAbsoluteEncoder();
-        /*
-         * algaeRanger = new LaserCan(AlgaeConstants.ALGAE_RANGER_ID);
-         * try {
-         * algaeRanger.setTimingBudget(LaserCan.TimingBudget.TIMING_BUDGET_20MS);
-         * }
-         * 
-         * catch (ConfigurationFailedException e) {
-         * System.out.println("LaserCan error " + e);
-         * }
-         */
-        pivotMotor.setCANTimeout(250);
-        intakeMotor.setCANTimeout(250);
+        if (AlgaeConstants.kIsEnabled) {
 
-        SparkMaxConfig algaeConfig = new SparkMaxConfig();
-        algaeConfig.voltageCompensation(AlgaeConstants.ALGAE_MOTOR_VOLTAGE_COMP);
-        algaeConfig.smartCurrentLimit(AlgaeConstants.ALGAE_MOTOR_CURRENT_LIMIT);
-        algaeConfig.idleMode(IdleMode.kCoast);
-        pivotMotor.configure(algaeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+            pivotMotor = new SparkMax(AlgaeConstants.PIVOT_MOTOR_ID, MotorType.kBrushless);
+            intakeMotor = new SparkFlex(AlgaeConstants.INTAKE_MOTOR_ID, MotorType.kBrushless);
+            pivotEncoder = pivotMotor.getAbsoluteEncoder();
+        
+            /*
+            * algaeRanger = new LaserCan(AlgaeConstants.ALGAE_RANGER_ID);
+             * try {
+            * algaeRanger.setTimingBudget(LaserCan.TimingBudget.TIMING_BUDGET_20MS);
+             * }
+             * 
+             * catch (ConfigurationFailedException e) {
+             * System.out.println("LaserCan error " + e);
+             * }
+             */
+            pivotMotor.setCANTimeout(250);
+            intakeMotor.setCANTimeout(250);
 
-        // According to Dan, a timing delay between config requests has been observed to
-        // correct heisenbugs
-        Timer.delay(0.1);
-        SparkFlexConfig intakeConfig = new SparkFlexConfig();
-        intakeConfig.voltageCompensation(AlgaeConstants.ALGAE_MOTOR_VOLTAGE_COMP);
-        intakeConfig.smartCurrentLimit(AlgaeConstants.ALGAE_MOTOR_CURRENT_LIMIT);
-        intakeConfig.idleMode(IdleMode.kBrake);
-        intakeMotor.configure(intakeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+            SparkMaxConfig algaeConfig = new SparkMaxConfig();
+            algaeConfig.voltageCompensation(AlgaeConstants.ALGAE_MOTOR_VOLTAGE_COMP);
+            algaeConfig.smartCurrentLimit(AlgaeConstants.ALGAE_MOTOR_CURRENT_LIMIT);
+            algaeConfig.idleMode(IdleMode.kCoast);
+            pivotMotor.configure(algaeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+            // According to Dan, a timing delay between config requests has been observed to
+            // correct heisenbugs
+            Timer.delay(0.1);
+            SparkFlexConfig intakeConfig = new SparkFlexConfig();
+            intakeConfig.voltageCompensation(AlgaeConstants.ALGAE_MOTOR_VOLTAGE_COMP);
+            intakeConfig.smartCurrentLimit(AlgaeConstants.ALGAE_MOTOR_CURRENT_LIMIT);
+            intakeConfig.idleMode(IdleMode.kBrake);
+            intakeMotor.configure(intakeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        }
     }
 
     public int distanceToAlgaeInMm() {
-        LaserCan.Measurement measurement = algaeRanger.getMeasurement();
-        if (measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT) {
-            return measurement.distance_mm;
+        if (AlgaeConstants.kIsEnabled) {
+            LaserCan.Measurement measurement = algaeRanger.getMeasurement();
+            if (measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT) {
+                return measurement.distance_mm;
+            }
+            return -1;
         }
-        return -1;
+        return 0;
     }
 
     public void runpivotmotor(double speed) {
-        pivotMotor.set(speed);
-    }
+        if (AlgaeConstants.kIsEnabled) {
+            pivotMotor.set(speed);
+        }
+    } 
+    
 
     public void runintakemotor(double speed) {
-        intakeMotor.set(speed);
+        if (AlgaeConstants.kIsEnabled) {
+            intakeMotor.set(speed);
+        }
     }
 
     public boolean algaeinrange() {
@@ -79,33 +91,43 @@ public class AlgaePickerSubsystem extends SubsystemBase {
     }
 
     public boolean arminintakeposition() {
-        double pivotposition = pivotEncoder.getPosition();
-        boolean pivotintakepart1 = pivotposition < AlgaeConstants.ALGAE_ARM_INTAKE_POSITION;
+        if (AlgaeConstants.kIsEnabled) {
+            double pivotposition = pivotEncoder.getPosition();
+            boolean pivotintakepart1 = pivotposition < AlgaeConstants.ALGAE_ARM_INTAKE_POSITION;
 
-        boolean pivotintakepart2 =  pivotposition > AlgaeConstants.ALGAE_ARM_INTAKE_POSITION + 0.5;
-       // SmartDashboard.putNumber("pivotposition",pivotposition );
-       // SmartDashboard.putNumber("pivotintakelimit2", AlgaeConstants.ALGAE_ARM_INTAKE_POSITION + 0.5);
-        //SmartDashboard.putBoolean("pivotintakepart1", pivotintakepart1);
-        //SmartDashboard.putBoolean("pivotintakepart2", pivotintakepart2);
-        //SmartDashboard.putBoolean("pivotinintakeposition", pivotintakepart1 || pivotintakepart2);
+            boolean pivotintakepart2 =  pivotposition > AlgaeConstants.ALGAE_ARM_INTAKE_POSITION + 0.5;
+            // SmartDashboard.putNumber("pivotposition",pivotposition );
+            // SmartDashboard.putNumber("pivotintakelimit2", AlgaeConstants.ALGAE_ARM_INTAKE_POSITION + 0.5);
+            //SmartDashboard.putBoolean("pivotintakepart1", pivotintakepart1);
+            //SmartDashboard.putBoolean("pivotintakepart2", pivotintakepart2);
+            //SmartDashboard.putBoolean("pivotinintakeposition", pivotintakepart1 || pivotintakepart2);
         
-        return pivotintakepart1 || pivotintakepart2;
+            return pivotintakepart1 || pivotintakepart2;
+        }
+        return false;
     }
 
     public boolean arminhomeposition() {
-        double pivotposition = pivotEncoder.getPosition();
-        boolean pivothomepart1 = pivotposition > AlgaeConstants.ALGAE_ARM_HOME_POSITION;
-        boolean pivothomepart2 = pivotposition < AlgaeConstants.ALGAE_ARM_HOME_POSITION + 0.5;
-        //SmartDashboard.putBoolean("pivothomepart1", pivothomepart1);
-        //SmartDashboard.putBoolean("pivothomepart2", pivothomepart2);
-        //SmartDashboard.putBoolean("pivotinhomeposition",pivothomepart1 && pivothomepart2);        
+        if (AlgaeConstants.kIsEnabled) {
+            double pivotposition = pivotEncoder.getPosition();
+            boolean pivothomepart1 = pivotposition > AlgaeConstants.ALGAE_ARM_HOME_POSITION;
+            boolean pivothomepart2 = pivotposition < AlgaeConstants.ALGAE_ARM_HOME_POSITION + 0.5;
+            //SmartDashboard.putBoolean("pivothomepart1", pivothomepart1);
+            //SmartDashboard.putBoolean("pivothomepart2", pivothomepart2);
+            //SmartDashboard.putBoolean("pivotinhomeposition",pivothomepart1 && pivothomepart2);        
         
-        return pivothomepart1 && pivothomepart2;
+            return pivothomepart1 && pivothomepart2;
+        }
+        return false;
     }
 
     public double getPivotAngle() {
-        return pivotEncoder.getPosition();
+        if (AlgaeConstants.kIsEnabled) {
+            return pivotEncoder.getPosition();
+        }
+        return 0;
     }
+
 
     public double getWrappedPivotAngle() {
         double position = getPivotAngle();
@@ -118,81 +140,90 @@ public class AlgaePickerSubsystem extends SubsystemBase {
     }
 
     public void setBrake(boolean enabled, boolean sync) {
-        SparkMaxConfig config = new SparkMaxConfig();
-        if (enabled) {
-            config
-                    .idleMode(IdleMode.kBrake);
-        }
+        if (AlgaeConstants.kIsEnabled) {
+            SparkMaxConfig config = new SparkMaxConfig();
+            if (enabled) {
+                config
+                        .idleMode(IdleMode.kBrake);
+            }
 
-        else {
-            config
-                    .idleMode(IdleMode.kCoast);
-        }
+            else {
+                config
+                        .idleMode(IdleMode.kCoast);
+            }
 
-        if (sync)
-        {
-            pivotMotor.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
-        }
-        else
-        {
-            pivotMotor.configureAsync(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+            if (sync)
+            {
+                pivotMotor.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+            }
+            else
+            {
+                pivotMotor.configureAsync(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+            }
         }
     }
 
-    public Command reachForAlgae(
-            AlgaePickerSubsystem algaeSubsystem) {
-        return Commands.startEnd(
-                () -> {pivotMotor.set(AlgaeConstants.ALGAE_ARM_REACH_SPEED);
-                //SmartDashboard.putBoolean("reachforAlgae", true);
-                },
-                () -> {pivotMotor.set(0);
-                //SmartDashboard.putBoolean("reachforAlgae", false);
-                },
-                algaeSubsystem)
-                .onlyWhile(
-                        () -> (arminintakeposition()));
-
+    public Command reachForAlgae(AlgaePickerSubsystem algaeSubsystem) {
+        if (AlgaeConstants.kIsEnabled) {
+            return Commands.startEnd(
+                    () -> {pivotMotor.set(AlgaeConstants.ALGAE_ARM_REACH_SPEED);
+                    //SmartDashboard.putBoolean("reachforAlgae", true);
+                    },
+                    () -> {pivotMotor.set(0);
+                    //SmartDashboard.putBoolean("reachforAlgae", false);
+                    },
+                    algaeSubsystem)
+                    .onlyWhile(
+                            () -> (arminintakeposition()));
+        }
+        return run (()->{});
     }
 
-    public Command holdAlgae(
-            AlgaePickerSubsystem algaeSubsystem, double intakespeed) {
-        return Commands.run(
-                () -> {
-                    double wrappedPivotAngle = algaeSubsystem.getWrappedPivotAngle();
-                    double extensionAngle = AlgaeConstants.ALGAE_ARM_HOME_POSITION - wrappedPivotAngle;
-                    double scalingFactor = extensionAngle / (AlgaeConstants.ALGAE_ARM_HOME_POSITION - AlgaeConstants.ALGAE_ARM_INTAKE_POSITION);
-                    //intakeMotor.set(AlgaeConstants.INTAKE_HOLD_MOTOR_SPEED);
-                    intakeMotor.set(intakespeed);
-                    pivotMotor.set((AlgaeConstants.PIVOT_HOLD_MOTOR_SPEED * scalingFactor) + 0.02);
-                    setBrake(false, false);
+    public Command holdAlgae(AlgaePickerSubsystem algaeSubsystem, double intakespeed) {
+        if (AlgaeConstants.kIsEnabled) {
+            return Commands.run(
+                    () -> {
+                        double wrappedPivotAngle = algaeSubsystem.getWrappedPivotAngle();
+                        double extensionAngle = AlgaeConstants.ALGAE_ARM_HOME_POSITION - wrappedPivotAngle;
+                        double scalingFactor = extensionAngle / (AlgaeConstants.ALGAE_ARM_HOME_POSITION - AlgaeConstants.ALGAE_ARM_INTAKE_POSITION);
+                        //intakeMotor.set(AlgaeConstants.INTAKE_HOLD_MOTOR_SPEED);
+                        intakeMotor.set(intakespeed);
+                        pivotMotor.set((AlgaeConstants.PIVOT_HOLD_MOTOR_SPEED * scalingFactor) + 0.02);
+                        setBrake(false, false);
                     
-                },
+                    },
                 algaeSubsystem);
+        }
+        return run(()->{});
     }
 
-    public Command returnArm(
-            AlgaePickerSubsystem algaeSubsystem) {
-        return Commands.startEnd(
-                () -> {
-                    pivotMotor.set(AlgaeConstants.ALGAE_ARM_RETURN_SPEED);
-                    intakeMotor.set(AlgaeConstants.INTAKE_HOLD_MOTOR_SPEED);
-                    setBrake(false, false);
-                    //SmartDashboard.putBoolean("ReturnArm", true);
-                },
-                () -> {pivotMotor.set(0);
-                //SmartDashboard.putBoolean("ReturnArm", false);
-                },
-                algaeSubsystem)
-                .onlyWhile(
-                        () -> (!arminhomeposition()));
+    public Command returnArm(AlgaePickerSubsystem algaeSubsystem) {
+        if (AlgaeConstants.kIsEnabled) {
+            return Commands.startEnd(
+                    () -> {
+                        pivotMotor.set(AlgaeConstants.ALGAE_ARM_RETURN_SPEED);
+                        intakeMotor.set(AlgaeConstants.INTAKE_HOLD_MOTOR_SPEED);
+                        setBrake(false, false);
+                        //SmartDashboard.putBoolean("ReturnArm", true);
+                    },
+                    () -> {pivotMotor.set(0);
+                    //SmartDashboard.putBoolean("ReturnArm", false);
+                    },
+                    algaeSubsystem)
+                    .onlyWhile(
+                            () -> (!arminhomeposition()));
+        }
+        return run(()->{});
     }
 
-    public Command scoreAlgae(
-            AlgaePickerSubsystem algaeSubsystem) {
-        return Commands.startEnd(
-                () -> intakeMotor.set(AlgaeConstants.SHOOT_MOTOR_SPEED),
-                () -> intakeMotor.set(0),
-                algaeSubsystem);
+    public Command scoreAlgae(AlgaePickerSubsystem algaeSubsystem) {
+        if (AlgaeConstants.kIsEnabled) {
+            return Commands.startEnd(
+                    () -> intakeMotor.set(AlgaeConstants.SHOOT_MOTOR_SPEED),
+                    () -> intakeMotor.set(0),
+                    algaeSubsystem);
+        }
+        return run(()->{}); 
     }
 
     @Override
